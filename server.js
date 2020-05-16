622881d7a3deee9ce99b662e435edc9877a427aa
gdreid13 <gdreid13@gmail.com>
2020-05-16 11:08:37 -0700

    removed console.logs

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(helmet())
 app.use(function ValidateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
-
-  console.log('validate bearer token middleware')
   
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
